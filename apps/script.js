13def411add80a211c9b42e2c4e6e5c32916379c
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2023-10-24 14:54:45 -0300

    pokedex

--- apps/script.js
+++ apps/script.js
@@ -1,1 +1,56 @@
-console.log("em breve")
+const pokemonName = document.getElementById("pokeName");
+const pokemonNumber = document.getElementById("pokeNum");
+const pokemonImage = document.getElementById("pokeImg");
+
+const form = document.querySelector('.form');
+const pokemonSearch = document.querySelector('.input-search');
+
+const btnPrev = document.getElementById("prevBtn");
+const btnNext = document.getElementById("nextBtn");
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
+};
+
+const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...';
+    const data = await fetchPokemon(pokemon);
+    if (data) {
+        pokemonImage.style.display="block"
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonSearch.value = "";
+        searchPokemon = data.id;-
+    } else {
+        pokemonImage.style.display="none"
+        pokemonName.innerHTML = "Not found :c";
+        pokemonNumber.innerHTML = "";
+    }
+};
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(pokemonSearch.value.toLowerCase())
+}); 
+
+btnPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+}); 
+
+btnNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+}); 
+
+renderPokemon(searchPokemon);
